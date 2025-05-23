33e2547acce768ca3300c4e00c23cf310f43f812
VoidKnight0011 <geoseffevincent@gmail.com>
2025-05-23 13:06:02 +0800

    V0.30 Added Toggleable Light/Dark Theme?

---
+++ code.js
@@ -0,0 +1,17 @@
+let Theme = false;
+const ThemeToggle = document.getElementById('Theme-Change');
+
+ThemeToggle.addEventListener("click", () => {
+  Theme = !Theme;
+  if(Theme) {
+    ThemeToggle.classList.remove("bxs-moon");
+    ThemeToggle.classList.add("bxs-sun");
+    document.body.classList.remove("theme-light");
+    document.body.classList.add("theme-dark");
+  } else {
+    ThemeToggle.classList.remove("bxs-sun");
+    ThemeToggle.classList.add("bxs-moon");
+    document.body.classList.remove("theme-dark");
+    document.body.classList.add("theme-light");
+  }
+});
